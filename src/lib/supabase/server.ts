eb9c194edc786510a1f1438f3f2bbd096829f420
Patrice Douge <patricedouge@gmail.com>
2025-01-04 23:11:48 -0500

    supabase setup

---
+++ src/lib/supabase/server.ts
@@ -0,0 +1,33 @@
+import "server-only";
+
+import { cookies } from "next/headers";
+import { createServerClient } from "@supabase/ssr";
+
+export function createClient() {
+  const cookieStore = cookies();
+
+  return createServerClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+    {
+      cookies: {
+        async getAll() {
+          const cookieStore = await cookies();
+          return cookieStore.getAll();
+        },
+        async setAll(cookiesToSet) {
+          const cookieStore = await cookies();
+          try {
+            cookiesToSet.forEach(({ name, value, options }) =>
+              cookieStore.set(name, value, options)
+            );
+          } catch {
+            // The `setAll` method was called from a Server Component.
+            // This can be ignored if you have middleware refreshing
+            // user sessions.
+          }
+        },
+      },
+    }
+  );
+}
